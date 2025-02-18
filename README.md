5114811326736631841fdff6e746ed25bfe430d7
Gudalol <gustavozabele10@gmail.com>
2025-02-18 14:08:22 -0300

    API finalizada

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# ProdutosAPI
+# Produtos API
 API de produtos totalmente em springboot
+
+Esse projeto foi com a finalidade de praticar e testar meus conhecimentos iniciais em springboot.
+
+# Como usar?
+
+É uma API bem simples, para utilizar basta clonar o repositório e rodar diretamente na porta 8080(padrão do tomcat). Tá sendo utilizando o h2 database, um banco de memória, para acessar esse banco basta acessar o localhost junto com /h2-console. Exemplo: http://localhost:8080/h2-console.
+
+Os dados sobre o banco e outras coisas sobre configuração está na pasta resources, nos respectivos arquivos application.yml e data.sql.
--- produtosapi/src/main/java/com/gferreira/produtosapi/controller/ProdutoController.java
+++ produtosapi/src/main/java/com/gferreira/produtosapi/controller/ProdutoController.java
@@ -2,11 +2,9 @@ package com.gferreira.produtosapi.controller;
 
 import com.gferreira.produtosapi.model.Produto;
 import com.gferreira.produtosapi.repository.ProdutoRepository;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -26,5 +24,27 @@ public class ProdutoController {
         produto.setId(id);
         produtoRepository.save(produto);
         return produto;
+    }
+
+    @GetMapping("/{id}")
+    public Produto obterPorID(@PathVariable("id") String id) {
+        return produtoRepository.findById(id).orElse(null);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteProduto(@PathVariable("id") String id) {
+        produtoRepository.deleteById(id);
+    }
+
+    @PutMapping("/{id}")
+    public void atualizarProduto(@PathVariable("id") String id,
+                                     @RequestBody Produto produto) {
+        produto.setId(id);
+        produtoRepository.save(produto);
+    }
+
+    @GetMapping
+    public List<Produto> buscarProdutos(@RequestParam("nome")String nome){
+        return produtoRepository.findByNome(nome);
     }
 }
